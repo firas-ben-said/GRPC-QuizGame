e1f2e760d6c1b1c024b7d0bd4816135b25b8cc28
firas-ben-said <firas.ben.said@horizon-tech.tn>
2024-12-16 03:50:22 +0100

    Minor change

--- src/main/java/org/example/QuizGameUI.java
+++ src/main/java/org/example/QuizGameUI.java
@@ -7,10 +7,8 @@ import quiz.QuizOuterClass;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class QuizGameUI extends JFrame {
     private ManagedChannel channel;
